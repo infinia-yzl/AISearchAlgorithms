f910e4c107ab34f3bdf94597b16c1ffa52f9b97e
KSing Liew <ksingl93@hotmail.com>
2013-10-19 22:57:36 +0800

    Update Node.java

--- Node.java
+++ Node.java
@@ -1,5 +1,7 @@
 /**
- * A Node that stores a coordinate.
+ * File Node.java
+ * 
+ * Represents a Node that stores a coordinate.
  * 
  * @author Isaac Yong
  * @version 15/10/2013
@@ -11,27 +13,56 @@ public class Node implements Comparable<Node> {
 
 	public Node() {}
 
+	/**
+	 * To assign the coordinates to a node
+	 * 
+	 * @param integer x for coordinate x  
+	 * @param integer y for coordinate y
+	 */
 	public Node(int x, int y) {
 		this.x = x;
 		this.y = y;
 	}
 
+	/**
+	 * To get the coordinate of x
+	 * 
+	 * @return coordinate of x
+	 */
 	public int getX() {
 		return this.x;
 	}
 
+	/**
+	 * To get the coordinate of y
+	 * 
+	 * @return coordinate of y
+	 */
 	public int getY() {
 		return this.y;
 	}
 
+	/**
+	 * To set the coordinate of x
+	 * 
+	 * @return coordinate of x
+	 */
 	public void setX(int x) {
 		this.x = x;
 	}
 
+	/**
+	 * To set the coordinate of y
+	 * 
+	 * @return coordinate of y
+	 */
 	public void setY(int y) {
 		this.y = y;
 	}
 
+	/**
+	 * To compare equality between two nodes
+	 */
 	public int compareTo(Node nodeB) {
 		if((this.x == nodeB.getX())&&(this.y == nodeB.getY())) {
 			return 1;
